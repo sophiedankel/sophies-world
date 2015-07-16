401488b913e50485b8c74258a2e213e082965c46
Sophie <sophiedankel@gmail.com>
2015-07-16 01:52:35 -0700

    Testing index.html

--- js/init.js
+++ js/init.js
@@ -1,4 +1,28 @@
-$(document).ready(function() {
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+
+loadScript("killring.js", myPrettyCode);
+loadScript("history.js", myPrettyCode);
+loadScript("readline.js", myPrettyCode);
+loadScript("shell.js", myPrettyCode);
+loadScript("pathhandler.js", myPrettyCode);
+loadScript("input.js", myPrettyCode);
+
+var init = $(document).ready(function() {
 	var history = new Josh.History({ key: 'josh.helloworld'});
 	var shell = Josh.Shell({history: history});
 	var promptCounter = 0;
